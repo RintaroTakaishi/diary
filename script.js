218c4b2058184fedfdf81a84ce34b3e1648cc95c
RintaroTakaishi <103246549+RintaroTakaishi@users.noreply.github.com>
2022-06-06 19:26:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 $(function(){
 
+  //水平線を引く
+  speed = 3000;
+  $('.horizon1').animate({
+    width:540
+  },speed);
+
+  //水平線を引く２
+  // $('.horizon2').css({
+  //   left: 540
+  // }).animate({
+  //   left: 0,
+  //   width: 1080
+  // },speed);
+
+  //カウントアップ
+    var countElm = $('.count'),
+    countSpeed = 30;
+    countElm.each(function(){
+      var self = $(this),
+      countMax = self.attr('data-num'),
+      thisCount = self.text(),
+      countTimer;
+      function timer(){
+        countTimer = setInterval(function(){
+          var countNext = thisCount++;
+          self.text(countNext);
+          if(countNext == countMax){
+            clearInterval(countTimer);
+
+            $('.count').fadeOut();
+            $('.horizon1').fadeOut();
+          }
+        },countSpeed);
+      }
+      timer();
+    });
+
+    $('.count').css({
+      left:290
+    }).animate({
+      left:830
+    },speed);
+
   // SNSボタン
   $('.social-icon').hover(
     function(){
@@ -13,21 +56,21 @@ $(function(){
       }, 300);
     }
   );
+
+
   
-  // トップへ戻るボタン
-  $('#top-btn').click(function(){
-    $('html, body').animate({ 
-      'scrollTop': 0 
-    }, 500);
+  //MENUボタン
+  $(".header-menu").on("click",function(){
+    $(".box").slideToggle();
   });
   
-  // ヘッダー内の<a>タグをクリックしたときのclickイベントを作成してください
-  $('header a').click(function(){
-    var id = $(this).attr('href');
-    var position = $(id).offset().top;
-    $('html, body').animate({
-      'scrollTop': position 
-    }, 500);
+  //tab切替
+  $(".tab").on("click", function() {
+    var i = $(this).index();
+    $(".works,.tab").removeClass("active");
+    $(".works").eq(i).addClass("active");
+    $(".tab").eq(i).addClass("active");
   });
-  
+
 });
+
