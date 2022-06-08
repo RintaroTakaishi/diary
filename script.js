9738805454e7d20114242aac10c2a966c7c4edd5
RintaroTakaishi <103246549+RintaroTakaishi@users.noreply.github.com>
2022-06-08 09:50:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,56 @@ $(function(){
     width:540
   },speed);
 
+  $('.count').css({
+    left:290
+  }).animate({
+    left:830
+  },speed);
+
+  $('.point').css({
+    left:265
+  }).animate({
+    left:805
+  },speed);
+
+  //boxを動かす
+
+  for (var i=1; i<4; i++) {
+    console.log(i);
+    $('.box1').animate({
+      left:950
+    },speed/i).animate({
+      left:30
+    },speed/i);
+ 
+    $('.box2').animate({
+      left:30
+    },speed).animate({
+      left:880
+    },speed);
+
+    $('.box3').animate({
+      left:200
+    },speed).animate({
+      left:400
+    },speed);
+
+    $('.box3').animate({
+      top:100
+    },speed).animate({
+      top:300
+    },speed);
+  }
+  // クリックボール
+  $(".click-ball").on("click",function(){
+    $(this).toggleClass('is-animated');
+    $(this).toggleClass('ball');
+  });
+
+
+
+
+
   //水平線を引く２
   // $('.horizon2').css({
   //   left: 540
@@ -28,20 +78,22 @@ $(function(){
           self.text(countNext);
           if(countNext == countMax){
             clearInterval(countTimer);
+            // countが100になった時の動作
+            $('.first-page').slideUp(2000);
+            $('.panish').animate({
+              backgroundColor:"#dedede",
+              opacity:0
+            },1500);
 
-            $('.count').fadeOut();
-            $('.horizon1').fadeOut();
+            $('.after-count').fadeIn(5000);
           }
         },countSpeed);
       }
       timer();
     });
 
-    $('.count').css({
-      left:290
-    }).animate({
-      left:830
-    },speed);
+
+
 
   // SNSボタン
   $('.social-icon').hover(
@@ -56,12 +108,10 @@ $(function(){
       }, 300);
     }
   );
-
-
   
   //MENUボタン
   $(".header-menu").on("click",function(){
-    $(".box").slideToggle();
+    $(".menu-box").slideToggle();
   });
   
   //tab切替
